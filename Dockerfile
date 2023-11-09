c516515fcb70cfb3da7b8ed672cec9aa59de1d88
vladimir lisovskij <work.vladimir.lisovskij@gmail.com>
2023-11-09 18:14:49 +0300

    added removed MOCK_DATABASE from Dockerfile; added host and port arguments to app module

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM amazoncorretto:17-alpine-jdk
 COPY ./app/build/distributions/app /app
-ENTRYPOINT /app/bin/app --database=MOCK_PATH
+ENTRYPOINT /app/bin/app
--- app/src/jvmMain/kotlin/tech/lsfohtmbm/app/Main.kt
+++ app/src/jvmMain/kotlin/tech/lsfohtmbm/app/Main.kt
@@ -8,16 +8,28 @@ import tech.lsfohtmbm.mainpage.mainPage
 import tech.lsfohtmbm.server.Server
 
 private const val ARG_DATABASE_PATH = "--database"
+private const val ARG_HOST = "--host"
+private const val ARG_PORT = "--port"
 
 fun main(arguments: Array<String>) {
     val (_, dataBasePath) = arguments
         .first { it.startsWith(ARG_DATABASE_PATH) }
         .split("=")
 
+    val (_, host) = arguments
+        .first { it.startsWith(ARG_HOST) }
+        .split("=")
+
+    val (_, port) = arguments
+        .first { it.startsWith(ARG_PORT) }
+        .split("=")
+
     Server(
         createDataBaseSource(dataBasePath),
         HTML::mainPage,
         HTML::articleListPage,
-        HTML::renderArticle
+        HTML::renderArticle,
+        host,
+        port.toInt()
     ).run()
 }

57cbe547046a4a77342168a40b0cdd9e6e58e095
vladimir lisovskij <work.vladimir.lisovskij@gmail.com>
2023-11-09 21:55:59 +0300

    fixed app arguments

--- app/src/jvmMain/kotlin/tech/lsfohtmbm/app/Main.kt
+++ app/src/jvmMain/kotlin/tech/lsfohtmbm/app/Main.kt
@@ -11,20 +11,24 @@ private const val ARG_DATABASE_PATH = "--database"
 private const val ARG_HOST = "--host"
 private const val ARG_PORT = "--port"
 
+private fun Array<String>.getArgument(argumentName: String): String? {
+    val argument = firstOrNull { it.startsWith(argumentName) } ?: return null
+    return runCatching {
+        val (_, value) = argument.split("=")
+        value
+    }.getOrNull()
+}
+
 fun main(arguments: Array<String>) {
     println(arguments.joinToString(separator = "+"))
+    val dataBasePath = arguments.getArgument(ARG_DATABASE_PATH)
+        ?: throw Exception("Unknown database path")
 
-    val (_, dataBasePath) = arguments
-        .first { it.startsWith(ARG_DATABASE_PATH) }
-        .split("=")
+    val host = arguments.getArgument(ARG_HOST)
+        ?: throw Exception("Unknown host")
 
-    val (_, host) = arguments
-        .first { it.startsWith(ARG_HOST) }
-        .split("=")
-
-    val (_, port) = arguments
-        .first { it.startsWith(ARG_PORT) }
-        .split("=")
+    val port = arguments.getArgument(ARG_PORT)
+        ?: throw Exception("Unknown port")
 
     Server(
         createDataBaseSource(dataBasePath),

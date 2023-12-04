d49b0bb4f38a1efbff8b4454e5d003e48a30805a
vladimir lisovskij <work.vladimir.lisovskij@gmail.com>
2023-12-04 19:24:31 +0300

    changed jdk configuration

--- build_logic/kotlin/src/main/kotlin/tech/lsfohtmbm/buildlogic/kotlin/jvm/Jvm.kt
+++ build_logic/kotlin/src/main/kotlin/tech/lsfohtmbm/buildlogic/kotlin/jvm/Jvm.kt
@@ -1,9 +1,12 @@
 package tech.lsfohtmbm.buildlogic.kotlin.jvm
 
+import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.kotlin.dsl.configure
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import tech.lsfohtmbm.buildlogic.kotlin.utils.JDK_VERSION
 import tech.lsfohtmbm.buildlogic.kotlin.utils.addKotlinMultiplatform
@@ -12,12 +15,24 @@ class Jvm : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             addKotlinMultiplatform(pluginManager)
-//            kotlinExtension.jvmToolchain(JDK_VERSION)
-//            tasks.withType(KotlinCompile::class.java) {
-//                compilerOptions {
-//                    jvmTarget.set(JvmTarget.fromTarget(JDK_VERSION.toString()))
-//                }
-//            }
+            extensions.configure<KotlinMultiplatformExtension> {
+                jvm {
+                    jvmToolchain(JDK_VERSION)
+
+                }
+            }
+
+            tasks.withType(KotlinCompile::class.java) {
+                compilerOptions {
+                    jvmTarget.set(JvmTarget.fromTarget(JDK_VERSION.toString()))
+                }
+            }
+
+            extensions.findByType(JavaPluginExtension::class.java)?.apply {
+                sourceCompatibility = JavaVersion.toVersion(JDK_VERSION)
+                targetCompatibility = JavaVersion.toVersion(JDK_VERSION)
+            }
+
         }
     }
 }
